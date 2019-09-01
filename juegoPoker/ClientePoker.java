c6b33841cc2bf0b89fde781a261dd77e8a6df394
FabianAU28 <52760957+FabianAU28@users.noreply.github.com>
2019-09-01 11:37:02 -0500

    Add files via upload

--- juegoPoker/ClientePoker.java
+++ juegoPoker/ClientePoker.java
@@ -1,5 +1,75 @@
 package juegoPoker;
 
-public class ClientePoker {
+import java.awt.BorderLayout;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ClientePoker extends JFrame implements Runnable{
+	private ArrayList<Jugador> jugadores;
+	private PanelCartas comunitario;
+	private JButton ver,retirar,pasar,ir;
+	private ServerSocket server;
+	private Socket sc;
+	private JOptionPane optionPane;
+	private String host;
+	private JPanel panelBotones;
+	private int puerto;
+	private DataOutputStream salida;
+	private DataInputStream entrada;
+	
+	public ClientePoker(String host,int PUERTO) {
+		host = host;
+		optionPane = new JOptionPane();
+		setSize(1200,700);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setTitle("Texas Hold´em");
+		jugadores = new ArrayList<Jugador>();
+		puerto = PUERTO;
+	}
+	
+		public void initGUI() {
+			panelBotones = new JPanel();
+			add(jugadores.get(0),BorderLayout.NORTH);
+			add(jugadores.get(1),BorderLayout.NORTH);
+			comunitario = new PanelCartas();
+			add(jugadores.get(2),BorderLayout.SOUTH);
+			add(jugadores.get(3),BorderLayout.SOUTH);
+			add(comunitario,BorderLayout.CENTER);
+			
+		}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			sc = server.accept();
+			optionPane.showMessageDialog(optionPane, "Has iniciado el juego");
+			salida = new DataOutputStream(sc.getOutputStream());
+			entrada = new DataInputStream(sc.getInputStream());
+			iniciarWhile();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void iniciarWhile() {
+		while(true) {
+			initGUI();
+		}
+	}
+	
 
 }
