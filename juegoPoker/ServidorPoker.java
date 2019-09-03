06c1e53442a27189a83fd7fae0740117481a4628
FabianAU28 <52760957+FabianAU28@users.noreply.github.com>
2019-09-03 10:05:09 -0500

    Add files via upload

--- juegoPoker/ServidorPoker.java
+++ juegoPoker/ServidorPoker.java
@@ -1,12 +1,35 @@
 package juegoPoker;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class ServidorPoker extends  Thread {
+public class ServidorPoker extends JFrame {
+    private ObjectInputStream in;
+    private ControlPoker control;
+    private int localHost;
+    private ExecutorService ejecutarJuego;
+    private Lock bloqueoJuego;
+    private Condition otroJugadorConeectado,turnoOtroJugador;
+    private int jugadorActual;
+    private ArrayList<Jugar> jugadores;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public ServidorPoker(){
+        ejecutarJuego = Executors.newFixedThreadPool(4);
+        bloqueoJuego = new ReentrantLock();
+        otroJugadorConeectado = bloqueoJuego.newCondition();
+        turnoOtroJugador = bloqueoJuego.newCondition();
 
-	}
+        jugadores = new ArrayList<Jugar>(4);
+    }
 
+    private class Jugar extends Jugador implements Runnable{
+
+    }
 }
